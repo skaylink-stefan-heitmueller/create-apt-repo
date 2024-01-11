5c4e41220c6efb644bb79ce54b37aed74ff1ca3a
Stefan Heitmüller <stefan.heitmueller@skaylink.com>
2024-01-11 19:59:02 +0100

    add import from repo
    Signed-off-by: Stefan Heitmüller <stefan.heitmueller@skaylink.com>

--- import.sh
+++ import.sh
@@ -9,7 +9,10 @@ fi
 
 (
     echo "set base_path /tmp/apt-mirror"
+    echo "set run_postmirror 0"
     echo "${IMPORT_FROM_REPO}"
 ) > /tmp/mirror.list
 apt-mirror /tmp/mirror.list
-cp -rv /tmp/apt-mirror/mirror/*/{dists,pool} .
+mapfile -t packages < <(find /tmp/apt-mirror/mirror -type f -name '*.deb')
+# shellcheck disable=SC2048,SC2086
+cp -v ${packages[*]} .
